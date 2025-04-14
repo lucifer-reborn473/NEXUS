76ba122ad86541c0213bee84a8f2eee80ffdf2a1
rohitt-raj <rajrohit@iitgn.ac.in>
2025-04-14 11:53:46 +0530

    Updated conftest (remove use of depreciated path args)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-
-def pytest_ignore_collect(path):
-    return str(path).endswith(('basic_test.py', 'operation_test.py', 'project_euler_codes.py'))
-
+# def pytest_ignore_collect(path):
+#     return str(path).endswith(('basic_test.py', 'operation_test.py', 'project_euler_codes.py'))
+def pytest_ignore_collect(collection_path):
+    return collection_path.name in ('basic_test.py', 'operation_test.py', 'project_euler_codes.py')
